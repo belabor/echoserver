d1ba82a51b60952d717a9dadc1541a39d30a8e0d
belabor <belabor@users.noreply.github.com>
2016-11-04 17:10:42 -0500

    More changes..

--- client.c
+++ client.c
@@ -1,53 +1,80 @@
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define MAX_BUFF 100
 
-int main(int argc,char **argv)
-{
+/*writes given message to local console */
+void print_to_console(char *buffer, int i) {
+  while (i > 0) {
+    char c = *buffer;
+    fprintf(stderr, "%c", c);
+    buffer++;
+    i--;
+  }
+}
+
+/* convert String to an Int without atoi */
+int string_to_int(const char *str) {
+  int val = 0;
+  while (*str) {
+    val = val * 10 + (*str++ - '0');
+  }
+  return val;
+}
+
+int main(int argc, char **argv) {
   int portNum = 0;
-  if ( argc != 2 ) {
+  if (argc == 2) {
+    portNum = (unsigned short)string_to_int(argv[1]);
+    printf("portNum: %d\n", portNum);
+  }
+  if (portNum == 0) {
     portNum = 22000; /* default to port 22000 */
-  } else {
-    portNum = atoi(argv[1]);
-  }
-  if (argc != 2) /* test for port number */
-  {
-    fprintf(stderr, "You must choose a server port.\n");
-    exit(1);
+    print_to_console("Defaulting to port 22000\n", 25);
   }
 
-    int sockfd = 0;
-    char sendline[MAX_BUFF];
-    char recvline[MAX_BUFF];
-    struct sockaddr_in servaddr;
+  int sockfd = 0;
+  char sendline[MAX_BUFF];
+  char recvline[MAX_BUFF];
+  struct sockaddr_in servaddr;
+  int readLength = 0;
+  int writeLength = 0;
 
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
-    bzero(&servaddr,sizeof servaddr);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  bzero(&servaddr, sizeof servaddr);
 
-    servaddr.sin_family=AF_INET;
+  servaddr.sin_family = AF_INET;
 
-    servaddr.sin_port=htons(portNum);
+  servaddr.sin_port = htons(portNum);
 
-    inet_pton(AF_INET,"127.0.0.1",&(servaddr.sin_addr));
+  /* This should probably ask for a specific address later */
+  inet_pton(AF_INET, "127.0.0.1", &(servaddr.sin_addr));
 
-    connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    print_to_console("Error establishing connection\n", 30);
+  };
 
-    while(1)
-    {
-        bzero( sendline, MAX_BUFF);
-        bzero( recvline, MAX_BUFF);
-        fgets(sendline,MAX_BUFF,stdin); /*stdin = 0 , for standard input */
+  while (1) {
+    bzero(sendline, MAX_BUFF);
+    bzero(recvline, MAX_BUFF);
+    fgets(sendline, MAX_BUFF, stdin); /*stdin = 0 , for standard input */
 
-        write(sockfd,sendline,strlen(sendline)+1);
-        read(sockfd,recvline,MAX_BUFF);
-        printf("%s",recvline);
+    writeLength = write(sockfd, sendline, strlen(sendline) + 1);
+    if (writeLength < 0) {
+      print_to_console("Error writing to socket\n", 24);
     }
-
+    readLength = read(sockfd, recvline, MAX_BUFF);
+    if (readLength < 0) {
+      print_to_console("Error reading socket\n", 21);
+    } else {
+      print_to_console("Server responded with: ", 23);
+      print_to_console(recvline, readLength);
+    }
+  }
 }
--- server.c
+++ server.c
@@ -1,37 +1,40 @@
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <errno.h>
+#include <math.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
 #define MAX_BUFF 100 /* max length of string */
 
-/* error handler using perror */
-void error_exit(char *msg) {
-  perror(msg);
-  exit(1);
-}
-
 /* simple time stamping */
 char *timestamp() {
-  char *stamp = (char *)malloc(sizeof(char) * 16);
-  time_t ltime;
-  ltime=time(NULL);
+  char *stamp = (char *)malloc(sizeof(char) * 14);
+  time_t ltime = time(NULL);
   struct tm *tm;
-  tm=localtime(&ltime);
+  tm = localtime(&ltime);
 
-  sprintf(stamp,"%04d%02d%02d%02d%02d%02d", tm->tm_year+1900, tm->tm_mon,
-  tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+  sprintf(stamp, "%04d%02d%02d%02d%02d%02d", tm->tm_year + 1900, tm->tm_mon,
+          tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
   return stamp;
 }
 
+/* convert String to an Int without atoi */
+int string_to_int(const char *str) {
+  int val = 0;
+  while (*str) {
+    val = val * 10 + (*str++ - '0');
+  }
+  return val;
+}
+
 /* Writes out given message to socket */
-int write_data(int socket, char* buffer, int i) {
+int write_data(int socket, char *buffer, int i) {
   int output, total = 0;
   while (total < i) {
     output = write(socket, buffer, i - total);
@@ -48,7 +51,7 @@ int write_data(int socket, char* buffer, int i) {
 }
 
 /*writes given message to local console */
-void print_to_console(char* buffer, int i) {
+void print_to_console(char *buffer, int i) {
   while (i > 0) {
     char c = *buffer;
     fprintf(stderr, "%c", c);
@@ -61,57 +64,63 @@ int main(int argc, char **argv) {
 
   /* setup the port number */
   int portNum = 0;
-  if ( argc <= 2 ) {
-    portNum = 22000;  /* default to port 22000 */
+  if (argc == 2) {
+    portNum = (unsigned short)string_to_int(argv[1]);
+    printf("portNum: %d\n", portNum);
+  }
+  if (portNum == 0) {
+    portNum = 22000; /* default to port 22000 */
     print_to_console("Defaulting to port 22000\n", 25);
-  } else {
-    portNum = atoi(argv[1]);
   }
 
   /* create a TCP Stream */
   int listen_fd = 0;
   listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (listen_fd < 0) return -1;
-  printf("Created TCP Stream\n");
+  if (listen_fd < 0)
+    return -1;
+  print_to_console("Created TCP Stream\n", 19);
 
   /* time to make the sockets */
   struct sockaddr_in servaddr;
   memset(&servaddr, 0, sizeof(servaddr));
-  printf("Createing socket\n");
 
+  print_to_console("Createing socket\n", 17);
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htons(INADDR_ANY);
   servaddr.sin_port = htons((unsigned short)portNum);
-  printf("Established!\n");
 
   /* bind the socket to servaddr */
-  if (bind(listen_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+  if (bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
     close(listen_fd);
     print_to_console("Something went wrong when binding to the socket\n", 48);
     return -1;
+  } else {
+    print_to_console("bound to the socket\n", 20);
   }
-  printf("bound to the socket\n");
 
   /* Now let's listen in.... */
   if (listen(listen_fd, 10) == -1) {
     close(listen_fd);
     print_to_console("Something went wrong when listening to port\n", 44);
     return -1;
+  } else {
+    print_to_console("Listening in...\n", 16);
   }
-  printf("Listening in...\n");
 
   /* create an infinite loop as they connect */
   struct sockaddr_in peer_addr;
   socklen_t addr_len = sizeof(peer_addr);
-  while(1) {
-    printf("Look (re)started\n");
-    int comm_fd = accept(listen_fd, (struct sockaddr*) &peer_addr, &addr_len);
+  while (1) {
+    print_to_console("Loop (re)started\n", 17);
+    int comm_fd = accept(listen_fd, (struct sockaddr *)&peer_addr, &addr_len);
     if (comm_fd == -1) {
-      error_exit("Problem with accepting connections on socket\n");
+      print_to_console("Socket is not established\n", 26);
+      break;
     }
     pid_t pid = fork();
     if (pid == -1) {
-      error_exit("Problem with forking.\n");
+      print_to_console("Problem with forking.\n", 22);
+      break;
     }
 
     /* which process is this??? */
@@ -121,26 +130,33 @@ int main(int argc, char **argv) {
       char str[MAX_BUFF];
       while (1) {
         /* set readLength to length of string */
-        int readLength = read(comm_fd,str,MAX_BUFF);
-        if ( readLength == -1 ) {
-          fprintf(stderr, "Error reading from client: %s\n", strerror(errno));
-          exit(-1);
+        int readLength = read(comm_fd, str, MAX_BUFF);
+        int sentLength;
+        if (readLength == -1) {
+          print_to_console("Error reading from client.\n", 27);
+          break;
         } else if (readLength == 0) {
-          fprintf(stderr, "Client has closed connection!\n");
+          print_to_console("Client has closed connection!\n", 30);
           close(comm_fd);
-          exit(0);
+          break;
         } else if (readLength > 0) {
           /* print out to console */
-          print_to_console(timestamp(), 16);
+          print_to_console(timestamp(), 14);
           print_to_console(" - ", 3);
           print_to_console(str, readLength);
 
           /* echo to client */
-          write_data(comm_fd, str, readLength);
+          sentLength = write_data(comm_fd, str, readLength);
+          if (sentLength > 0) { /* Do noting, everything is ok */
+          } else if (sentLength < 0) {
+            print_to_console("Problem with sending to client!\n", 32);
+          } else {
+            print_to_console("Nothing sent to client (possible error).\n", 41);
+          }
 
           /* log to file */
           FILE *fp;
-          fp = fopen("log.txt","a");
+          fp = fopen("log.txt", "a");
           fprintf(fp, "%s - %s", timestamp(), str);
           fclose(fp);
         }
@@ -148,7 +164,9 @@ int main(int argc, char **argv) {
     } else if (pid > 0) {
       /* This is the parent */
       close(comm_fd);
+      print_to_console("Parent fork established\n", 24);
     }
   }
+  print_to_console("Fork closed\n", 12);
   return 0;
 }
